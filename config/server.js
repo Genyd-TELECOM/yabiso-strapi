15eabdd93d047a5093d47e134fc3aba6e1f97481
mechack tambwe <gustave@pavillonnoir.com>
2022-06-27 02:15:00 +0100

    feat: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e4e16aecd64a08e58ba8e3b72bb364fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fde556f3-eea4-4383-a196-e9f916de530b'
+};
